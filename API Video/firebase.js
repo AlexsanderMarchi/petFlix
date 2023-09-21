5107b9b52712a4f57268c9bcef0acc0de209f9a7
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2023-09-21 15:50:40 -0300

    feat: connect to firebase storage

---
+++ API Video/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "petflix-839d0.firebaseapp.com",
+  projectId: "petflix-839d0",
+  storageBucket: "petflix-839d0.appspot.com",
+  messagingSenderId: "38217464403",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- API Video/server.js
+++ API Video/server.js
@@ -1,16 +1,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = 3000;
+const path = require("path");
+const fs = require("fs");
+const { Storage } = require("@google-cloud/storage");
 
-const path = require("path")
-const fs = require("fs")
+const app = express();
+app.use(bodyParser.json()); // sem isso, não é possível enviar requisições em JSON para a API
 
-const app = express()
-app.use(bodyParser.json())  //sem isso, não da pra mandar requisição em json para a API
+const storage = new Storage({
+  projectId: 'petflix-839d0',
+  keyFilename: 'firabase.js', // Substitua pelo caminho para o seu arquivo de chave de serviço do Firebase
+});
+
+const bucketName = 'petflix-839d0.appspot.com'; // Substitua pelo nome do seu bucket
+
+app.get("/video", (req, res) => {
+  const arquivoDoStorage = "gs://petflix-839d0.appspot.com/Hakuna Matata.mp4"; // Substitua pelo caminho correto no Firebase Storage
+
+  const file = storage.bucket(bucketName).file(arquivoDoStorage);
+
+  // Verifique se o arquivo existe no Firebase Storage
+  file.exists().then((data) => {
+    const [exists] = data;
+
+    if (!exists) {
+      return res.status(404).send("Arquivo não encontrado");
+    }
+
+    // Buscar detalhes do arquivo
+    file.getMetadata().then((metadata) => {
+      const tamanhoDoVideo = metadata[0].size;
+      const cabecalhoDaResposta = {
+        'Content-Length': tamanhoDoVideo,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(200, cabecalhoDaResposta);
+
+      // Lendo o arquivo do Firebase Storage e retornando-o como resposta
+      file.createReadStream().pipe(res);
+    });
+  }).catch((error) => {
+    console.error("Erro durante a verificação do arquivo:", error);
+    res.status(500).send("Erro interno do servidor");
+  });
+});
+
+
+
 
 const pastaVideo = path.join(__dirname, "videos")
 
-app.get("/video", (req, res) => {
+app.get("/videoLocal", (req, res) => {
     const arquivoDoVideo = path.join(pastaVideo, "seuVideo.mp4")
 
     // Verificar se o arquivo existe
@@ -35,45 +77,51 @@ app.get("/video", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Servidor express rodando na porta: ${port}`)
-})
+  console.log(`Servidor express rodando na porta: ${port}`);
+});
 
 
-/*const bodyParser = require("body-parser");
-const port = 3000;
-
-const path = require("path")
-const fs = require("fs")
 
 
-const app = express()
-app.use(bodyParser.json())  //sem isso, não da pra mandar requisição em json para a API
 
-const pastaVideo = path.join(__dirname, "videos")
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const port = 3000;
 
-app.get("/video", (req, res) => {
-    const arquivoDoVideo = path.join(pastaVideo, "seuVideo.mp4")
+// const path = require("path")
+// const fs = require("fs")
 
-    // Verificar se o arquivo existe
-    fs.existsSync(arquivoDoVideo)
+// const app = express()
+// app.use(bodyParser.json())  //sem isso, não da pra mandar requisição em json para a API
 
-    // Buscar detalhes do arquivo
-    const detalhesDoArquivo = fs.statSync(arquivoDoVideo)
-    // Pegar somente o  tamanho do arquivo
-    const tamanhoDoVideo = detalhesDoArquivo.size
+// const pastaVideo = path.join(__dirname, "videos")
 
-    const cabecalhoDaResposta = {
-        'Content-Length': tamanhoDoVideo,
-        'Content-Type': 'video/mp4'
-    }
+// app.get("/video", (req, res) => {
+//     const arquivoDoVideo = path.join(pastaVideo, "seuVideo.mp4")
 
-    res.writeHead(200, cabecalhoDaResposta)
+//     // Verificar se o arquivo existe
+//     fs.existsSync(arquivoDoVideo)
 
-    // Lendo o arquivo numa stream e retornando ele como resposta
-    fs.createReadStream(arquivoDoVideo)   
-    .pipe(res)    
+//     // Buscar detalhes do arquivo
+//     const detalhesDoArquivo = fs.statSync(arquivoDoVideo)
+//     // Pegar somente o  tamanho do arquivo
+//     const tamanhoDoVideo = detalhesDoArquivo.size
 
-})
+//     const cabecalhoDaResposta = {
+//         'Content-Length': tamanhoDoVideo,
+//         'Content-Type': 'video/mp4'
+//     }
 
-app.listen(port, () => {
-    console.log(`Servidor express rodando na porta: ${port}`)*/
+//     res.writeHead(200, cabecalhoDaResposta)
+
+//     // Lendo o arquivo numa stream e retornando ele como resposta
+//     fs.createReadStream(arquivoDoVideo)   
+//     .pipe(res)    
+
+// })
+
+// app.listen(port, () => {
+//     console.log(`Servidor express rodando na porta: ${port}`)
+// })
+
+
