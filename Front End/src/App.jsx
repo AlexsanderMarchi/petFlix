3c8e7a9ac3943e4e54bef8d150a0f72ea677da64
Alexsander <alexsandermarchi@gmail.com>
2023-09-20 21:11:38 -0300

    feat:create basic front functions

---
+++ Front End/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+import PetflixHeader from '../components/Header';
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+    <PetflixHeader />
+    <main>
+      {}
+    </main>
+  </div>
+  );
+}
+
+export default App;
