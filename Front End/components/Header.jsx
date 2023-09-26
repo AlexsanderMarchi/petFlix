43699501bea8a4f5c87a9f22285b2714e09b5b35
Alexsander <alexsandermarchi@gmail.com>
2023-09-25 22:00:57 -0300

    feat: video from storage playing in front

--- Front End/components/Header.jsx
+++ Front End/components/Header.jsx
@@ -4,14 +4,14 @@ function PetflixHeader() {
   return (
     <div className="petflix-header">
       <div className='menu-itens'>
-      <img
-        className="petflix-logo"
-        src="./public/siteLogo.png" 
-        alt="Petflix Logo"
-      />
-<div className='button-row'>
-      <button className="petflix-sign-in">Movies</button>
-      </div>
+        <img
+          className="petflix-logo"
+          src="./public/siteLogo.png"
+          alt="Petflix Logo"
+        />
+        <div className='button-row'>
+          <button className="petflix-sign-in">Movies</button>
+        </div>
       </div>
     </div>
   );
