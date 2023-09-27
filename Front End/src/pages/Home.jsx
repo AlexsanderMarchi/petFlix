918d99d0f881bc8da889fae082d9284889e9af26
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2023-09-27 19:26:20 -0300

    style: switch profile title position

--- Front End/src/pages/Home.jsx
+++ Front End/src/pages/Home.jsx
@@ -1,50 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
-import './home.css';
-import dogZao from '../assets/dogZao.png';
-import catZao from '../assets/catZao.png'
+  import React, { useState, useEffect } from 'react';
+  import { Link } from "react-router-dom";
+  import './home.css';
+  import dogZao from '../assets/dogZao.png';
+  import catZao from '../assets/catZao.png'
 
-function Home() {
-  const [showDogTitle, setShowDogTitle] = useState(false);
-  const [showCatTitle, setShowCatTitle] = useState(false);
+  function Home() {
+    const [showDogTitle, setShowDogTitle] = useState(false);
+    const [showCatTitle, setShowCatTitle] = useState(false);
 
 
 
-  return (
-    <div className='container'>
-      <div className='logo-container'>
-        <img
-          className="petflix-logo"
-          src="./public/siteLogo.png"
-          alt="Petflix Logo"
-        />
-      </div>
-      <div className='image-container'>
-        <div
-          onMouseEnter={() => setShowDogTitle(true)}
-          onMouseLeave={() => setShowDogTitle(false)}
-          className="image-container"
-        >
-          {showDogTitle && <div className='dogProfileTitle left'>Filmes para Cachorros</div>}
-          <Link to={`/CatalogForDogs`}>
-            <img className="dogProfile" src={dogZao} alt="" />
-          </Link>
+    return (
+      <div className='container'>
+        <div className='logo-container'>
+          <img
+            className="petflix-logo"
+            src="./public/siteLogo.png"
+            alt="Petflix Logo"
+          />
         </div>
-        <div
-          onMouseEnter={() => setShowCatTitle(true)}
-          onMouseLeave={() => setShowCatTitle(false)}
-          className="image-container"
-        >
-          {showCatTitle && <div className='dogProfileTitle right'>Filmes para Gatos</div>}
-          <Link to={`/CatalogForCats`}>
-            <img className="catProfile" src={catZao} alt="" />
-          </Link>
+        <div className='image-container'>
+          <div
+            onMouseEnter={() => setShowDogTitle(true)}
+            onMouseLeave={() => setShowDogTitle(false)}
+            className="image-container"
+          >
+            {showDogTitle && <div className='dogProfileTitle left'>Cachorro</div>}
+            <Link to={`/CatalogForDogs`}>
+              <img className="dogProfile" src={dogZao} alt="" />
+            </Link>
+          </div>
+          <div
+            onMouseEnter={() => setShowCatTitle(true)}
+            onMouseLeave={() => setShowCatTitle(false)}
+            className="image-container"
+          >
+            {showCatTitle && <div className='dogProfileTitle right'>Gato</div>}
+            <Link to={`/CatalogForCats`}>
+              <img className="catProfile" src={catZao} alt="" />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Home;
+  export default Home;
 
 
